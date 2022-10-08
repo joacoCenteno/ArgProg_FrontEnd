0cc20c04f04c2cac22e3abaeb6e422ec55a1992e
Centeno Joaquin <centenojoaco01@gmail.com>
2022-10-08 20:05:32 -0300

    Se añadieron datos al datos.json

--- Modulo3_Argentina_Programa/src/app/principal/projects/projects.component.ts
+++ Modulo3_Argentina_Programa/src/app/principal/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-projects',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-
-  constructor() { }
+  principalProjects:any;
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      this.principalProjects = data.projects;
+    })
   }
 
 }
--- Modulo3_Argentina_Programa/src/app/principal/skills/skills.component.ts
+++ Modulo3_Argentina_Programa/src/app/principal/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-skills',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-
-  constructor() { }
+  principalSkills:any;
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      this.principalSkills = data.skills;
+    })
   }
 
 }
